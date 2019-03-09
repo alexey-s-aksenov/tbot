c035c0785348c507f4d5d4f123296020eaa8bdb5
masterl <alexey.s.aksenov@gmail.com>
2019-03-09 18:14:02 +0300

    Updated to the new bash.im interface

--- joke/joke.go
+++ joke/joke.go
@@ -5,19 +5,13 @@ package joke
 import (
 	"bytes"
 	"errors"
-	"fmt"
-	"html"
-	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
-	"regexp"
+	"sort"
 	"strconv"
-	"strings"
 
 	xhtml "golang.org/x/net/html"
-
-	"github.com/djimenez/iconv-go"
 )
 
 // func main() {
@@ -29,146 +23,118 @@ type quote struct {
 	text   string
 }
 
-func (q quote) String() string {
-	return fmt.Sprintf("rating: %v, text: %v", q.rating, q.text)
-}
-
 type bashorg struct {
 }
 
-// GetJokeBash получает html по ссылке и возвращает цитату с наивысшим рейтингом
-func (j *bashorg) GetJoke() (string, error) {
-	site := "https://bash.im/random"
+type getWebRequest interface {
+	FetchBytes(url string) ([]byte, error)
+}
+
+type liveGetWebRequest struct {
+}
+
+func (liveGetWebRequest) FetchBytes(url string) ([]byte, error) {
 	resp, err := http.Get(site)
 	if err != nil {
 		log.Printf("joke.go: Error in GetJokeBash func: %s", err)
-		return "", err
+		return nil, err
 	}
 
 	defer resp.Body.Close()
-	//body, err := ioutil.ReadAll(resp.Body)
 
-	utfBody, err := iconv.NewReader(resp.Body, "windows-1251", "utf-8")
-	if err != nil {
-		log.Printf("joke.go: Error converting ccsid to UTF: %s", err)
-		return "", nil
-	}
-	//buf := make([]byte, 100)
-	re := regexp.MustCompile(`\r?\n`)
-	reT := regexp.MustCompile(`\t`)
+	// re := regexp.MustCompile(`\r?\n`)
+	// reT := regexp.MustCompile(`\t`)
 
-	buf, err := ioutil.ReadAll(utfBody)
+	buf, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		log.Printf("joke.go: Error reading from reader: %s", err)
-		return "", nil
+		return nil, err
 	}
-	input := re.ReplaceAllString(string(buf), "")
-	input = reT.ReplaceAllString(input, "")
 
-	doc, err := xhtml.Parse(strings.NewReader(input))
+	return buf, nil
+}
 
-	joke, err := getQuotes(doc)
+// GetJokeBash получает html по ссылке и возвращает цитату с наивысшим рейтингом
+func (*bashorg) GetJoke() (string, error) {
+	request := liveGetWebRequest{}
+	return innerGetJoke(request)
+}
+
+func innerGetJoke(request getWebRequest) (string, error) {
+	site := "https://bash.im/random"
+
+	buf, err := request.FetchBytes(site)
+	if err != nil {
+		return "", err
+	}
+	root, err := xhtml.Parse(bytes.NewReader(buf))
+
+	joke, err := getQuotes(root)
 	if err != nil {
 		log.Printf("joke.go: Error while parsing html: %s", err)
 		return "", nil
 	}
 	// fmt.Println(quotes)
 	return joke, nil
+}
 
+func getQuotesSlice(doc *xhtml.Node) []*xhtml.Node {
+	var result = make([]*xhtml.Node, 0)
+	var f func(n *xhtml.Node)
+	f = func(n *xhtml.Node) {
+		if n.Type == xhtml.ElementNode && n.Data == "article" && checkAttr(n, "class", "quote") {
+			result = append(result, n)
+		}
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			f(c)
+		}
+	}
+	f(doc)
+	return result
+}
+
+func extractData(nn []*xhtml.Node) []*quote {
+	var result = make([]*quote, 0)
+	for _, n := range nn {
+		var q = new(quote)
+		var f func(n *xhtml.Node)
+		f = func(n *xhtml.Node) {
+			if n.Type == xhtml.ElementNode &&
+				n.Data == "div" &&
+				checkAttr(n, "class", "quote__body") {
+				var quote string
+				for c := n.FirstChild; c != nil; c = c.NextSibling {
+					quote = quote + c.Data + "\n"
+				}
+			}
+			if n.Type == xhtml.ElementNode && n.Data == "div" && checkAttr(n, "class", "quote__total") {
+				q.rating, _ = strconv.ParseInt(n.FirstChild.Data, 10, 32)
+			}
+			for c := n.FirstChild; c != nil; c = c.NextSibling {
+				f(c)
+			}
+		}
+		f(n)
+		if q != nil {
+			result = append(result, q)
+		}
+	}
+	return result
 }
 
 func getQuotes(root *xhtml.Node) (string, error) {
 
-	body, err := getBody(root)
-	if err != nil {
-		log.Printf("joke.go: Error in getQuotes func: %s", err)
-		return "", err
+	qq := extractData(getQuotesSlice(root))
+
+	if len(qq) == 0 {
+		return "", errors.New("joke.go: No quotes found")
 	}
 
-	body, err = getDivBody(body)
-	if err != nil {
-		log.Printf("joke.go: Error in getQuotes func: %s", err)
-		return "", err
-	}
-	m, err := getQuotesList(body)
+	sort.SliceStable(qq, func(i, j int) bool {
+		return qq[i].rating > qq[j].rating
+	})
 
-	return getTheBest(m), nil
-}
-
-//  get html <body>
-func getBody(doc *xhtml.Node) (*xhtml.Node, error) {
-	var b *xhtml.Node
-	var f func(*xhtml.Node)
-	f = func(n *xhtml.Node) {
-		if n.Type == xhtml.ElementNode && n.Data == "body" {
-			b = n
-		}
-		for c := n.FirstChild; c != nil; c = c.NextSibling {
-			f(c)
-		}
-	}
-	f(doc)
-	if b != nil {
-		return b, nil
-	}
-	return nil, errors.New("Missing <body> in the node tree")
-}
-
-// get <div id="body"
-func getDivBody(doc *xhtml.Node) (*xhtml.Node, error) {
-	var b *xhtml.Node
-	var f func(*xhtml.Node)
-	f = func(n *xhtml.Node) {
-		if n.Type == xhtml.ElementNode && n.Data == "div" && checkAttr(n, "id", "body") {
-			b = n
-		}
-		for c := n.FirstChild; c != nil; c = c.NextSibling {
-			f(c)
-		}
-	}
-	f(doc)
-	if b != nil {
-		return b, nil
-	}
-	return nil, errors.New("Missing <div id=body> in the node tree")
-}
-
-// create slice of quotes from <div class="quote">
-func getQuotesList(doc *xhtml.Node) ([]*quote, error) {
-	s := make([]*quote, 0)
-
-	for c := doc.FirstChild; c != nil; c = c.NextSibling {
-		if c.Type == xhtml.ElementNode && c.Data == "div" && checkAttr(c, "class", "quote") {
-			quot := new(quote)
-			// walk through all tags inside each <div class="quote">
-			for q := c.FirstChild; q != nil; q = q.NextSibling {
-				// geting rating
-				if q.Type == xhtml.ElementNode && q.Data == "div" && checkAttr(q, "class", "actions") {
-					for qq := q.FirstChild; qq != nil; qq = qq.NextSibling {
-						if qq.Type == xhtml.ElementNode && qq.Data == "span" && checkAttr(qq, "class", "rating-o") {
-							//fmt.Println(qq.FirstChild.FirstChild.Data)
-							quot.rating, _ = strconv.ParseInt(qq.FirstChild.FirstChild.Data, 10, 32)
-						}
-					}
-				}
-				// getting quote it self
-				if q.Type == xhtml.ElementNode && q.Data == "div" && checkAttr(q, "class", "text") {
-					//xtml fails to parse correctly
-					dirtyText := nodeToString(q)
-					end := strings.Index(dirtyText, "</div>")
-					dirtyText = dirtyText[len("<div class=\"text\">"):end]
-					dirtyText = html.UnescapeString(dirtyText)
-					quot.text = strings.Replace(dirtyText, "<br/>", "\n", -1)
-				}
-			}
-			s = append(s, quot)
-			if quot.rating <= 0 || quot.text == "" {
-				fmt.Println("!!!!!!!!!!!!!", nodeToString(c))
-			}
-		}
-	}
-
-	return s, nil
+	return qq[0].text, nil
 }
 
 func checkAttr(n *xhtml.Node, att string, val string) bool {
@@ -177,25 +143,12 @@ func checkAttr(n *xhtml.Node, att string, val string) bool {
 			return true
 		}
 	}
-
 	return false
 }
 
-func nodeToString(doc *xhtml.Node) string {
-	var buf bytes.Buffer
-	w := io.Writer(&buf)
-	xhtml.Render(w, doc)
-	return buf.String()
-}
-
-func getTheBest(q []*quote) string {
-	var result string
-	var found int64
-	for _, qq := range q {
-		if qq.rating > found {
-			result = qq.text
-			found = qq.rating
-		}
-	}
-	return result
-}
+// func nodeToString(doc *xhtml.Node) string {
+// 	var buf bytes.Buffer
+// 	w := io.Writer(&buf)
+// 	xhtml.Render(w, doc)
+// 	return buf.String()
+// }
--- joke/joke_test.go
+++ joke/joke_test.go
@@ -1,18 +1,244 @@
 package joke
 
 import (
-	"strconv"
+	"strings"
 	"testing"
+
+	xhtml "golang.org/x/net/html"
 )
 
-func TestGetTheBest(t *testing.T) {
-	testArray := make([]*quote, 0)
-	for _, s := range []int{3, 5, 4, 2, 1} {
-		testArray = append(testArray, &quote{int64(s), strconv.Itoa(s)})
+// func TestGetTheBest(t *testing.T) {
+// 	testArray := make([]*quote, 0)
+// 	for _, s := range []int{3, 5, 4, 2, 1} {
+// 		testArray = append(testArray, &quote{int64(s), strconv.Itoa(s)})
+// 	}
+// 	actual := getTheBest(testArray)
+// 	expected := "5"
+// 	if actual != expected {
+// 		t.Errorf("Wrong result. Expected: %v. Received: %v.\nTest array:\n%v", expected, actual, testArray)
+// 	}
+// }
+
+// func TestGetJoke(t *testing.T) {
+// 	// j := NewJokeGetter()
+// 	quote, err := NewJokeGetter().GetJoke()
+// 	if err != nil {
+// 		t.Error("Error: ", err)
+// 	} else {
+// 		t.Log("Resulting quote: ", quote)
+// 	}
+
+// }
+
+func TestExtractData(t *testing.T) {
+	const page = `<article class="quote" data-quote="397779">
+	<div class="quote__frame">
+	  <header class="quote__header">
+		<a class="quote__header_permalink" href="/quote/397779">#397779</a>
+		<div class="quote__header_date">
+		  11.07.2008 в 18:28
+		</div>
+	  </header>
+	  <div class="quote__body">
+		fool panda<br>нахуя бля я работаю по такой дерьмовой профессии
+		<br>
+		<br>Shenter<br>не говори так
+		<br>
+		<br>Shenter<br>ты же не какой-нибудь дизайнер
+		<br>
+		<br>fool panda<br>=-O ]:-&gt;<br>
+		<br>08.07.2008 14:51:45, Shenter<br>э... Ты дизайнер что ли? :-[
+		<br>
+	  </div>
+	  <footer class="quote__footer">
+		<div class="quote__button" role="button" aria-label="Это баян!" tabindex="0" data-vote="[397779, 2, 0]"
+			 title="Это баян!">
+		  <svg class="quote__dismiss" xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="none"
+			   stroke-width="2" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+			   aria-hidden="true">
+			<path d="M21 18H18V32H21"></path>
+			<circle cx="22" cy="23" r="1" fill="currentColor" stroke="none"></circle>
+			<circle cx="22" cy="27" r="1" fill="currentColor" stroke="none"></circle>
+			<path d="M30 17V33"></path>
+			<path d="M26 17V33"></path>
+			<circle cx="34" cy="23" r="1" fill="currentColor" stroke="none"></circle>
+			<circle cx="34" cy="27" r="1" fill="currentColor" stroke="none"></circle>
+			<path d="M35 18H38V32H35"></path>
+		  </svg>
+		</div>
+		<div class="quote__vote">
+		  <div class="quote__vote_button" role="button" aria-label="Голосовать против этой цитаты" tabindex="0"
+			   data-vote="[397779, 1, 0]" data-swipe="1" title="Голосовать против этой цитаты">
+			<svg class="quote__vote_icon down" xmlns="http://www.w3.org/2000/svg" width="30" height="30"
+				 fill="currentColor" fill-rule="evenodd" clip-rule="evenodd" aria-hidden="true">
+			  <path d="M9 14h12v2H9z"></path>
+			</svg>
+		  </div>
+		  <div class="quote__total" data-vote-counter>7728</div>
+		  <div class="quote__vote_button" role="button" aria-label="Голосовать за эту цитату" tabindex="0"
+			   data-vote="[397779, 0, 0]" data-swipe="0" title="Голосовать за эту цитату">
+			<svg class="quote__vote_icon up" xmlns="http://www.w3.org/2000/svg" width="30" height="30"
+				 fill="currentColor" fill-rule="evenodd" clip-rule="evenodd" aria-hidden="true">
+			  <path d="M16 14v-4h-2v4h-4v2h4v4h2v-4h4v-2h-4z"></path>
+			</svg>
+		  </div>
+		</div>
+		<div role="button" class="quote__button" role="button" aria-label="Поделиться" tabindex="0"
+			 data-share='{"id": "397779", "url": "quote/397779", "title": "Цитата #397779", "description": "fool panda&lt;br&gt;нахуя бля я работаю по такой дерьмовой профессии&lt;br&gt;&lt;br&gt;Shenter&lt;br&gt;не говори так&lt;br&gt;&lt;br&gt;Shenter&lt;br&gt;ты же не какой-нибудь дизайнер&lt;br&gt;&lt;br&gt;fool panda&lt;br&gt;=-O ]:-&amp;gt;&lt;br&gt;&lt;br&gt;08.07.2008 14:51:45, Shenter&lt;br&gt;э... Ты дизайнер что ли? :-[&lt;br&gt;"}'
+			 title="Поделиться цитатой">
+		  <svg class="quote__share" xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="none"
+			   stroke-width="2" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+			   aria-hidden="true">
+			<path d="M18.5 21l3.5-3.5 3.5 3.5M22 18v10M13 25v7h18v-7"></path>
+		  </svg>
+		</div>
+	  </footer>
+	  <div class="quote__overlay">
+		<svg class="quote__overlay_icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30"
+			 fill="currentColor" fill-rule="evenodd" clip-rule="evenodd" aria-hidden="true">
+		  <path class="down" d="M9 14h12v2H9z"></path>
+		  <path class="up" d="M16 14v-4h-2v4h-4v2h4v4h2v-4h4v-2h-4z"></path>
+		</svg>
+	  </div>
+	</div>
+  </article>`
+
+	doc, _ := xhtml.Parse(strings.NewReader(page))
+	var qq = make([]*xhtml.Node, 0)
+	//t.Log("testData", nodeToString(doc))
+	qq = append(qq, doc)
+	actual := extractData(qq)
+	var expected = new(quote)
+	expected.rating = 7728
+	expected.text = `fool panda
+нахуя бля я работаю по такой дерьмовой профессии
+
+Shenter
+не говори так
+
+Shenter
+ты же не какой-нибудь дизайнер
+
+fool panda
+=-O ]:-&gt;
+
+08.07.2008 14:51:45, Shenter
+э... Ты дизайнер что ли? :-[
+`
+	expected.text = xhtml.UnescapeString(expected.text)
+	if actual == nil {
+		t.Fatal("extractQuotes get no resulsts")
+	} else {
+		if expected.rating != actual[0].rating &&
+			expected.text != actual[0].text {
+			t.Errorf("Result mismatch\nExpected: r=%v, q=%v\nGot: r=%v, q=%v",
+				expected.rating, expected.text,
+				actual[0].text, actual[0].rating)
+		}
 	}
-	actual := getTheBest(testArray)
-	expected := "5"
-	if actual != expected {
-		t.Errorf("Wrong result. Expected: %v. Received: %v.\nTest array:\n%v", expected, actual, testArray)
+}
+
+type fakeGetWebRequest struct {
+}
+
+func (fakeGetWebRequest) FetchBytes(url string) ([]byte, error) {
+
+	return []byte(`<!doctype html>
+	<html theme="light" lang="ru" data-controller="random" class="">
+	<head>
+	  <meta charset="utf-8">
+	  <title>Подборка случайных цитат – Цитатник Рунета</title>
+	</head>
+	<body data-turbolinks-suppress-warning>
+	  <div class="columns">
+		<main class="columns__main ">
+		  <section class="quotes" data-page="">
+			<article class="quote" data-quote="405110">
+			  <div class="quote__frame">
+				<div class="quote__body">
+				  3<br>3<br>3
+				</div>
+				<footer class="quote__footer">
+	
+				  <div class="quote__vote">
+					<div class="quote__total" data-vote-counter>33</div>
+				  </div>
+				</footer>
+			  </div>
+			</article>
+			<article class="quote" data-quote="405110">
+			  <div class="quote__frame">
+				<div class="quote__body">
+				  4<br>4<br>4<br>4
+				</div>
+				<footer class="quote__footer">
+	
+				  <div class="quote__vote">
+					<div class="quote__total" data-vote-counter>4444</div>
+				  </div>
+				</footer>
+			  </div>
+			</article>
+			<article class="quote" data-quote="418738">
+			  <div class="quote__frame">
+				<header class="quote__header">
+				  <a class="quote__header_permalink" href="/quote/418738">#418738</a>
+				  <div class="quote__header_date">
+					04.09.2012 в 9:45
+				  </div>
+				</header>
+				<div class="quote__body">
+				  5<br>5<br>5<br>5<br>5
+				</div>
+				<footer class="quote__footer">
+				  <div class="quote__vote">
+					<div class="quote__vote_button" role="button" aria-label="Голосовать против этой цитаты" tabindex="0"
+						 data-vote="[418738, 1, 0]" data-swipe="1" title="Голосовать против этой цитаты">
+					  <svg class="quote__vote_icon down" xmlns="http://www.w3.org/2000/svg" width="30" height="30"
+						   fill="currentColor" fill-rule="evenodd" clip-rule="evenodd" aria-hidden="true">
+						<path d="M9 14h12v2H9z"></path>
+					  </svg>
+					</div>
+					<div class="quote__total" data-vote-counter>555</div>
+				  </div>
+				</footer>
+			  </div>
+			</article>
+		  </section>
+	
+	
+		  <div class="more">
+			<a href="/random?4197" class="more__link">
+			  Хочу ещё!
+			</a>
+		  </div>
+		</main>
+	  </div>
+	  <footer class="footer">
+		<nav class="footer__nav">
+		  <a href="/faq" class="footer__nav_link ">
+			О сайте
+		  </a>
+		  <a href="/webmaster" class="footer__nav_link ">
+			Вебмастеру
+		  </a>
+		  <a href="/rss" class="footer__nav_link">RSS</a>
+		  <a href="/rss/comics.xml" class="footer__nav_link">RSS комиксов</a>
+		</nav>
+	  </footer>
+	</body>
+	</html>
+	`), nil
+}
+
+func TestInnerGetJoke(t *testing.T) {
+	client := fakeGetWebRequest{}
+	expected := `4
+4
+4
+4`
+	actual, _ := innerGetJoke(client)
+	if actual == expected {
+		t.Errorf("Wrong result. Want: %v, Got: %v", expected, actual)
 	}
 }
